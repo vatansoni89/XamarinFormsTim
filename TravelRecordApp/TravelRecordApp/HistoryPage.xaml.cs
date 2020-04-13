88f4730e1ba1c6fae6ebf5be71ab3b6c28565d17
Vatan Soni <vatansoni89@gmail.com>
2020-04-14 02:55:42 +0530

    Read from database.

--- TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,16 @@ namespace TravelRecordApp
         public HistoryPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+
+            var posts = conn.Table<Post>().ToList();
+            conn.Close();
         }
     }
 }
