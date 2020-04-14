d62c7813d0f4e590b0f652f057252657ba84e0b8
Vatan Soni <vatansoni89@gmail.com>
2020-04-14 21:11:20 +0530

    Update and Delete ListView items (db operation update delete)

--- TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
@@ -29,5 +29,14 @@ namespace TravelRecordApp
                 postListView.ItemsSource = posts;
             }
         }
+
+        private void postListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedPost = postListView.SelectedItem as Post;
+            if (selectedPost!=null)
+            {
+                Navigation.PushAsync(new PostDetailPage(selectedPost));
+            }
+        }
     }
 }
---
+++ TravelRecordApp/TravelRecordApp/PostDetailPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostDetailPage : ContentPage
+    {
+        Post postDetail;
+        public PostDetailPage(Post post)
+        {
+            InitializeComponent();
+            postDetail = post;
+
+            experienceEntry.Text = post.Experience;
+        }
+
+        private void btnUpdate_Clicked(object sender, EventArgs e)
+        {
+            postDetail.Experience = experienceEntry.Text;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Update(postDetail);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Success", "Experience successfully updated", "Ok");
+                }
+                else
+                {
+                    DisplayAlert("Failure", "Failed to update", "Ok");
+                }
+                
+            }
+        }
+
+        private void btnDelete_Clicked(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Delete(postDetail);
+
+                if (rows > 0)
+                {
+                    experienceEntry.Text = string.Empty;
+                    DisplayAlert("Success", "Experience successfully deleted", "Ok");
+                }
+                else
+                {
+                    DisplayAlert("Failure", "Failed to delete", "Ok");
+                }
+                
+            }
+        }
+    }
+}
